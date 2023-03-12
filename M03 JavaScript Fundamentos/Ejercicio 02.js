1965284c5735a6c7090bef3b066f69cec45b7d23
jhanjo <johncuesta7rgmail.com>
2023-03-12 16:46:34 -0500

    se modifico operaciones basicas

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,31 +1,42 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
+function devolverString(perro= "no hay mensaje") {
    // Debe retornar un string.
    // Tu código:
+   let bienvenido="te damos la bienvenida";
+
+   console.log  (bienvenido +" "+ perro  );
+
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
+   let z = x+y;
+   return(z) ;
    // Tu código:
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+  return  x-y;
+
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x/y
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
+
    // Tu código:
+   return x*y;
+
 }
 
 function obtenerResto(x, y) {
@@ -42,3 +53,22 @@ module.exports = {
    multiplica,
    obtenerResto,
 };
+
+let resultado ;
+
+resultado = suma( 8, 9);
+console.log(resultado);
+
+let resultado2= "";
+devolverString ( "celular");
+ let resultado3 ;
+ resultado3 = resta (8,2);
+ console.log (resultado3);
+
+ let resultado4 ;
+ resultado4 = multiplica (8,5);
+ console.log (resultado4);
+
+let resultado5 ;
+resultado5 = divide (144/2,8);
+console.log (resultado5)
